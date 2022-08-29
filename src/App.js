bf2c0423d1ec76e5048e5b1d1b615d95e9b22d8b
ganeshsadhashivam <109951960+ganeshsadhashivam@users.noreply.github.com>
2022-08-29 18:47:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+//var userName = "Ganesh";
+//var userName = prompt("enter username");
+const emojiDict = {
+  "😀": "HappyFace",
+  "🤣": "Rolling on the Floor Laughing",
+  "😞": "Disappointed Face",
+  "✈️": "Airplane",
+  "📓": "Notebook",
+  "🌏": "Globe",
+  "🌼": "Flower",
+  "🎁": "Gift",
+  "🛕": "HinduTemple",
+  "🚒": "FireEngine"
+};
+
+//we converted emojiDict object to Array
+var emojiArraykeys = Object.keys(emojiDict);
+
+export default function App() {
+  //var [userInput, setUserInput] = useState("");
+
+  //function setEventHandler(event) {
+  // console.log(event.target);
+  //  console.log(event.target.value);
+
+  // setUserInput(event.target.value);
+
+  var [userInput, setUserInput] = useState("");
+  function setEventHandler(event) {
+    //console.log(event.target.value);
+    var emojiInput = event.target.value;
+
+    var meaning = emojiDict[emojiInput];
+
+    if (userInput === undefined) {
+      meaning = "we dont have this in our database";
+    }
+
+    //console.log(meaning);
+    setUserInput(meaning);
+  }
+
+  function emojiClicked(items) {
+    var meaning = emojiDict[items];
+    //console.log(items);
+    setUserInput(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emojipedia</h1>
+      <input onChange={setEventHandler} />
+      <h2>{userInput}</h2>
+
+      <h3>Emojis we Know</h3>
+
+      {emojiArraykeys.map((items) => {
+        return (
+          <span
+            onClick={() => emojiClicked(items)}
+            key={items}
+            style={{ padding: "1rem", fontSize: "2rem", cursor: "pointer" }}
+          >
+            {items}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
